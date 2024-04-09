2bef4674cae11ed27c2ff686a49a9920019d0628
Rob Montero <rob@zivtech.com>
2024-04-09 17:24:22 -0600

    Fix Image

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
-      <img src="/svg/brho-us-logo.svg" alt="BRHO US Logo" width={200} height="auto" />
+      <Image src="/svg/brho-us-logo.svg" alt="BRHO US Logo" width={200} height="auto" />
       <p className="text-2xl dark:text-white text-center">
         <Link href="/">
           <a>{name}</a>
